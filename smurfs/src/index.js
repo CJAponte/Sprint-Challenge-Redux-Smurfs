3ee8b207cbf0029b4fba4fb2d452f6b2de42b92f
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-06-21 13:45:44 -0400

    completed mvp. GET and POST work as required, completed DELETE function to remove smurf, completed PUT function to update smurf. Styled.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import Reducer from './reducers';
+import Reducer from './reducers/index';
 
 const middleware = applyMiddleware(logger, thunk);
 const store = createStore(Reducer, middleware);
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
