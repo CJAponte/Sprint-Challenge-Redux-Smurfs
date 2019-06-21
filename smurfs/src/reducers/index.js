cd62bcfda6e10d14d6c99e962f443e1a081be4df
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-06-21 12:18:00 -0400

    reducer set up. error message on screen resolved.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -6,22 +6,18 @@ const initialState = {
   loading: false,
 }
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+export default (state = initialState, action) => {
+  switch(action.type){
+    case LOADING:
+      return Object.assign({}, state, {loading: true})
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+    case GET_SMURFS:
+      return Object.assign({}, state, {smurfs: action.smurfs, loading: false, error: ''})
+
+    case ERROR_MESSAGE:
+      return Object.assign({}, state, {error: action.errorMessage, loading: false})
+
+    default:
+      return state 
+  }
+}
